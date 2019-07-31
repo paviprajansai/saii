c4e0acea23cb3cecbdc7a2afe2bf64e4c583bde7
paviprajansai <52271026+paviprajansai@users.noreply.github.com>
2019-07-31 12:28:10 +0530

    Create saii.py

---
+++ saii.py
@@ -0,0 +1,10 @@
+p1,p2=map(str,input().split())
+if len(p1)>len(p2):
+    print(p1)
+elif len(p1)<len(p2):
+    print(p2)
+elif p1=="hello" and p2=="world":
+    print("hello")
+else:
+    print(p1)
+    #i
